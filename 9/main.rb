1769ecfac5fd001124020ce1488874564551b78b
Scheals <95084627+scheals@users.noreply.github.com>
2022-12-09 18:32:31 +0100

    refactor: move catchup to Tail

--- 9/main.rb
+++ 9/main.rb
@@ -15,7 +15,7 @@ class NodePlane
     starting_node = Node.new(Coordinate.new(0, 0))
     @nodes = [starting_node]
     @head = Head.new(starting_node)
-    @tail = Tail.new(starting_node)
+    @tail = Tail.new(starting_node, head)
     starting_node.contents.push(head, tail)
   end
 
@@ -25,29 +25,21 @@ class NodePlane
 
   def move(direction, amount)
     amount.times do
-      head.move(navigate_to_node(direction), direction)
+      head.move(navigate_to_node(head, direction), direction)
       unless tail.current_node.adjacent?(head.current_node) || tail.current_node.contents.include?(head)
-        catchup
+        tail.catchup(navigate_to_node(tail.parent, REVERSE_DIRECTIONS[head.last_move]))
       end
     end
   end
 
-  def navigate_to_node(direction)
-    new_coordinate = head.current_node.coordinate.send(direction)
+  def navigate_to_node(parent, direction)
+    new_coordinate = parent.current_node.coordinate.send(direction)
     existing_node = find(new_coordinate)
     return existing_node if existing_node
 
     fresh_node = Node.new(new_coordinate)
     nodes << fresh_node
     fresh_node
-  end
-
-  def catchup
-    new_destination = navigate_to_node(REVERSE_DIRECTIONS[head.last_move])
-    tail.current_node.contents.delete(tail)
-    tail.current_node.contents.push(tail)
-    tail.current_node = new_destination
-    tail.visited_nodes << new_destination
   end
 
   def parse_input(input)
@@ -125,16 +117,25 @@ class Head
 end
 
 class Tail
-  attr_reader :visited_nodes
+  attr_reader :visited_nodes, :parent
   attr_accessor :current_node
 
-  def initialize(current_node)
+  def initialize(current_node, parent)
     @current_node = current_node
     @visited_nodes = [current_node]
+    @parent = parent
   end
 
   def visit_node(node)
     visited_nodes.push(node)
+  end
+
+  def catchup(node)
+    new_destination = node
+    current_node.contents.delete(self)
+    current_node.contents.push(self)
+    @current_node = new_destination
+    @visited_nodes << new_destination
   end
 end
 
