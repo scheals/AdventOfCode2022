db809e861cd28f442272834d04476601bf29657e
Scheals <95084627+scheals@users.noreply.github.com>
2022-12-13 14:38:52 +0100

    wip: gave up

--- 13/main.rb
+++ 13/main.rb
@@ -1,79 +1,71 @@
-
-class Packet
-  attr_reader :array
-
-  def initialize(list_string)
-    @array = dangerous_parse(list_string)
-  end
-
-  def flat?
-    return true if array == array.flatten
-
-    false
-  end
-
-  def shorter?(packet)
-    array.length < packet.array.length
-  end
-
-  def smaller?(packet)
-    array.any? do |number|
-      packet.each do |other_number|
-        return true if number < other_number
-      end
-    end
-  end
-
-  def dangerous_parse(line)
-    eval line
-  end
+def dangerous_parse(line)
+  eval line
 end
 
-def right_order?(first_packet, second_packet)
-  first_packet.array.each_with_index do |element, i|
-    other_element = second_packet.array[i]
-    if element.is_a?(Numeric) && other_element.is_a?(Numeric)
-      return false if element > second_packet.array[i]
-    elsif element.is_a?(Array) && other_element.is_a?(Array)
-      return compare?(element, other_element)
-    elsif element.is_a?(Integer)
-      element = [element]
-      return compare?(element, other_element)
-    elsif other_element.is_a?(Integer)
-      other_element = [other_element]
-      return compare?(element, other_element)
-    end
-  end
-  true
-end
+# def right_order?(first_packet, second_packet)
+#   first_packet.each_with_index do |element, i|
+#     return false unless second_packet
+#     return false unless second_packet[i]
 
-def compare?(parent_element, other_parent_element)
-  parent_element.each_with_index do |element, i|
-    return false unless other_parent_element
-    return false unless other_parent_element[i]
+#     other_element = second_packet[i]
+#     if both_numbers?(element, other_element)
+#       return false if element > second_packet[i]
+#     elsif both_arrays?(element, other_element)
+#       right_order?(element, other_element)
+#     elsif element.is_a?(Numeric)
+#       element = [element]
+#       right_order?(element, other_element)
+#     elsif other_element.is_a?(Numeric)
+#       other_element = [other_element]
+#       right_order?(element, other_element)
+#     end
+#   end
+#   true
+# end
 
-    other_element = other_parent_element.is_a?(Numeric) ? other_parent_element : other_parent_element[i]
-    if element.is_a?(Numeric) && other_element.is_a?(Numeric)
-      return false if element > other_parent_element[i]
-    elsif element.is_a?(Array) && other_element.is_a?(Array)
-      return compare?(element, other_element)
-    elsif element.is_a?(Integer)
-      element = [element]
-      return compare?(element, other_element)
-    elsif other_element.is_a?(Integer)
-      other_element = [other_element]
-      return compare?(element, other_element)
+def right_order?(first, second)
+  return true if first.empty?
+
+  first.each_with_index do |left, i|
+    return false unless second[i] # If the right list runs out of items first, the inputs are not in the right order.
+
+    right = second[i]
+    if both_numbers?(left, right) # If both values are integers, the lower integer should come first.
+      next if left == right # Otherwise, the inputs are the same integer; continue checking the next part of the input.
+
+      return left < right # If the left integer is lower than the right integer, the inputs are in the right order.
+      # If the left integer is higher than the right integer, the inputs are not in the right order.
+    elsif both_arrays?(left, right) # If both values are lists,
+      next if right_order?(left, right) # If the lists are the same length and no comparison makes a decision about the order, continue checking the next part of the input.
+
+      return false
+    elsif left.is_a?(Numeric) # If exactly one value is an integer,
+      next if right_order?([left], right) # convert the integer to a list which contains that integer as its only value, then retry the comparison.
+
+      return false
+    elsif right.is_a?(Numeric) # If exactly one value is an integer,
+      next if right_order?(left, [right]) # convert the integer to a list which contains that integer as its only value, then retry the comparison.
+
+      return false
     end
   end
   true
 end
 
-input = (File.readlines(ARGV[0], chomp: true) - ['']).map { |line| Packet.new(line) }
+def both_numbers?(a, b)
+  a.is_a?(Numeric) && b.is_a?(Numeric)
+end
+
+def both_arrays?(a, b)
+  a.is_a?(Array) && b.is_a?(Array)
+end
+
+input = (File.readlines(ARGV[0], chomp: true) - ['']).map { |line| dangerous_parse(line) }
 results = []
 input.each_slice(2) do |pair|
   first_packet = pair.first
   second_packet = pair.last
-  results << { right_order: right_order?(first_packet, second_packet) }
+  results << right_order?(first_packet, second_packet)
 end
-
-p results.filter_map.with_index { |result, i| i + 1 if result[:right_order] }
+p results
+p results.filter_map.with_index { |result, i| i + 1 if result } #4831 was too small #6772 was too high
